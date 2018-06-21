db7306c67d7f1a8ed9553fb62354260fdf1716a0
Callum Seymour <31986834+CallumSeymour@users.noreply.github.com>
2018-06-21 15:46:54 +0100

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,27 +1,21 @@
-// Map properties, incl. location
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(51.508742, -0.120850),
-        zoom: 5,
-        mapTypeId: google.maps.MapTypeId.HYBRID //Map type - roadmap, hybrid, satellite or terrain maps possible.
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
 
 
-// Map marker with animation
-var marker = new google.maps.Marker({
-    position: myCenter,
-    animation: google.maps.Animation.BOUNCE
-});
 
-marker.setMap(map);
+//Retrieve Lat./Long. from JSON feed. Then update map.
+//function getValue() {
+////    var res;
+//    $.ajax({
+//        type: 'GET',
+//        dataType: 'jsonp',
+//        url: 'http://api.open-notify.org/iss-now.json',
+//        async: false,
+//        crossDomain: true,
+//        complete: function (data) {
+//            if (data.readyState === 4 && data.status === 200) {
+//                Lat = data.responseJSON.iss_position.latitude;
+//                Long = data.responseJSON.iss_position.longitude;
+//            }
+//        }
+//    });
+//}
 
-// Zoom to map point when clicking on marker
-google.maps.event.addListener(marker, 'click', function () {
-    map.setZoom(9);
-    map.setCenter(marker.getPosition());
-    window.setTimeout(function () {
-        map.setZoom(pos);
-    }, 3000); //get map back after 3 seconds
-});

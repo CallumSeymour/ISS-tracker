af7909df9c3b0afec8cf4173d89ecd70079a9ac7
Callum Seymour <31986834+CallumSeymour@users.noreply.github.com>
2018-06-22 18:39:32 +0100

    Add files via upload

---
+++ Project3-master/myscript.js
@@ -0,0 +1,21 @@
+
+
+
+//Retrieve Lat./Long. from JSON feed. Then update map.
+//function getValue() {
+////    var res;
+//    $.ajax({
+//        type: 'GET',
+//        dataType: 'jsonp',
+//        url: 'http://api.open-notify.org/iss-now.json',
+//        async: false,
+//        crossDomain: true,
+//        complete: function (data) {
+//            if (data.readyState === 4 && data.status === 200) {
+//                Lat = data.responseJSON.iss_position.latitude;
+//                Long = data.responseJSON.iss_position.longitude;
+//            }
+//        }
+//    });
+//}
+
